f00798999fdea79e2764b5258b0feda87e214d68
ioancallay <ivanar71@uniandes.edu.ec>
2024-12-16 12:56:47 -0500

    update

--- app/src/main/java/com/example/app_calificaciones/resIng.kt
+++ app/src/main/java/com/example/app_calificaciones/resIng.kt
@@ -1,6 +1,9 @@
 package com.example.app_calificaciones
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,23 @@ class resIng : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val puntosActuales = intent.getIntExtra("puntosActuales", 0)
+        val nombre = intent.getStringExtra("nombre")
+
+        val lblNombre = findViewById<TextView>(R.id.lbl_ingnombre)
+        val lblRespuesta = findViewById<TextView>(R.id.lbl_ingresp)
+
+        lblNombre.text = nombre + " tus resultados"
+        lblRespuesta.text = "Puntos obtenidos " + puntosActuales.toString() + "\nMalas\nBuenas"
+        val btnRegresar = findViewById<Button>(R.id.btn_ingregresar)
+
+        btnRegresar.setOnClickListener {
+            val opciones = Intent(this, Activity_Options::class.java)
+            opciones.putExtra("nombre", nombre)
+            opciones.putExtra("puntosTotal", puntosActuales)
+            startActivity(opciones)
         }
     }
 }
